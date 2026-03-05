bbddf7a3f43e7f1daa43cd5802c81a5f188be732
Mher Hunanyan <mher.hunanyan2@gmail.com>
2026-03-05 17:37:51 +0400

    format fix

--- algorithms/sumUnderEachOther.js
+++ algorithms/sumUnderEachOther.js
@@ -1,39 +1,39 @@
 const sumTwoNumbers = (num1, num2) => {
- const longNumLength = Math.max(num1.length, num2.length);
+  const longNumLength = Math.max(num1.length, num2.length);
 
- let long;
- let short;
+  let long;
+  let short;
 
- if (num1.length > num2.length) {
-  long = num1.split("");
-  short = num2.split("");
- } else {
-  long = num2.split("");
-  short = num1.split("");
- }
-
- let keepNumber = false;
- let sum = "";
-
- for (let i = 0; i < longNumLength; ++i) {
-  const hasBottomNum = i < short.length;
-
-  let currSum = hasBottomNum
-   ? Number(long[long.length - 1 - i]) + Number(short[short.length - 1 - i])
-   : Number(long[long.length - 1 - i]);
-
-  if (keepNumber) {
-   currSum += 1;
-   keepNumber = false;
-  }
-
-  if (currSum < 10) {
-   sum = String(currSum) + sum;
+  if (num1.length > num2.length) {
+    long = num1.split("");
+    short = num2.split("");
   } else {
-   sum = String(currSum - 10) + sum;
-   keepNumber = true;
+    long = num2.split("");
+    short = num1.split("");
   }
- }
+
+  let keepNumber = false;
+  let sum = "";
+
+  for (let i = 0; i < longNumLength; ++i) {
+    const hasBottomNum = i < short.length;
+
+    let currSum = hasBottomNum
+      ? Number(long[long.length - 1 - i]) + Number(short[short.length - 1 - i])
+      : Number(long[long.length - 1 - i]);
+
+    if (keepNumber) {
+      currSum += 1;
+      keepNumber = false;
+    }
+
+    if (currSum < 10) {
+      sum = String(currSum) + sum;
+    } else {
+      sum = String(currSum - 10) + sum;
+      keepNumber = true;
+    }
+  }
 
  if (keepNumber) return "1" + sum;
 
